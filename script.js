00b9f3cca117050b7a41f74645393fd6955cdf2a
Damianen <113175544+Damianen@users.noreply.github.com>
2023-11-23 13:36:17 +0100

    add function to add new book cards to main div

--- script.js
+++ script.js
@@ -14,3 +14,18 @@ function addBookToLibrary(book) {
 function removeBookFromLibrary(book) {
     myLibrary.pop(book);
 }
+
+function addNewCard(book) {
+    const main = document.appendChild(".main");
+    const card = document.createElement("div");
+    
+    card.classList.add("book-card");
+    card.appendChild(document.createElement("h1").classList.add("book-title").textContent = `${book.title}`);
+    card.appendChild(document.createElement("p").textContent = `${book.description}`);
+    card.appendChild(document.createElement("p").textContent = `${book.author}`);
+    card.appendChild(document.createElement("p").textContent = `${book.pages}`)
+    card.appendChild(document.createElement("button").classList.add("unread").textContent = "unread");
+    card.appendChild(document.createElement("button").classList.add("remove").textContent = "remove");
+    
+    main.appendChild(card);
+}
