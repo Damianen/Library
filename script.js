79916f82a8b883163e006252f71cc3ff9059fa5b
Damianen <113175544+Damianen@users.noreply.github.com>
2023-11-23 14:29:21 +0100

    Fixed the add new book card function

--- script.js
+++ script.js
@@ -16,16 +16,36 @@ function removeBookFromLibrary(book) {
 }
 
 function addNewCard(book) {
-    const main = document.appendChild(".main");
+    const main = document.querySelector(".main");
     const card = document.createElement("div");
     
     card.classList.add("book-card");
-    card.appendChild(document.createElement("h1").classList.add("book-title").textContent = `${book.title}`);
-    card.appendChild(document.createElement("p").textContent = `${book.description}`);
-    card.appendChild(document.createElement("p").textContent = `${book.author}`);
-    card.appendChild(document.createElement("p").textContent = `${book.pages}`)
-    card.appendChild(document.createElement("button").classList.add("unread").textContent = "unread");
-    card.appendChild(document.createElement("button").classList.add("remove").textContent = "remove");
     
+    const title = document.createElement("h1");
+    title.textContent = `${book.title}`;
+    card.appendChild(title);
+    
+    const desc = document.createElement("p");
+    desc.textContent = `${book.description}`;
+    card.appendChild(desc);
+    
+    const author = document.createElement("p");
+    author.textContent = `${book.author}`;
+    card.appendChild(author);
+    
+    const pages = document.createElement("p");
+    pages.textContent = `${book.pages}`;
+    card.appendChild(pages);
+    
+    const btn1 = document.createElement("button");
+    btn1.textContent = "Not read";
+    btn1.className = "unread";
+    card.appendChild(btn1);
+    
+    const btn2 = document.createElement("button")
+    btn2.textContent = "remove";
+    btn2.className = "remove";
+    card.appendChild(btn2);    
+
     main.appendChild(card);
 }
