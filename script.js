3a8ef604f396c7d9810e32fcd30a5148b2b31401
Damianen <113175544+Damianen@users.noreply.github.com>
2023-11-23 14:48:09 +0100

    Add a way to remove books

--- script.js
+++ script.js
@@ -5,21 +5,26 @@ function Book(title, description, author, pages) {
     this.description = description;
     this.author = author;
     this.pages = pages;
+    this.read = false;
 }
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
+    addNewCard(book);
 }
 
 function removeBookFromLibrary(book) {
     myLibrary.pop(book);
+    const card = document.querySelector(`.${book.title}`);
+    card.remove();
 }
 
 function addNewCard(book) {
-    const main = document.querySelector(".main");
     const card = document.createElement("div");
-    
+    const main = document.querySelector(".main");
+
     card.classList.add("book-card");
+    card.classList.add(`${book.title}`);
     
     const title = document.createElement("h1");
     title.textContent = `${book.title}`;
@@ -40,12 +45,28 @@ function addNewCard(book) {
     const btn1 = document.createElement("button");
     btn1.textContent = "Not read";
     btn1.className = "unread";
+    btn1.addEventListener("click", () => {
+        if(book.read) { 
+            btn1.classList.remove("read");
+            btn1.classList.add("unread"); 
+            book.read = false;
+        } else { 
+            btn1.classList.remove("unread");
+            btn1.classList.add("read");
+            book.read = true;
+        }
+    });
     card.appendChild(btn1);
-    
+
     const btn2 = document.createElement("button")
     btn2.textContent = "remove";
     btn2.className = "remove";
+    btn2.addEventListener("click", () => {
+        removeBookFromLibrary(book);
+    });
     card.appendChild(btn2);    
 
     main.appendChild(card);
 }
+
+addBookToLibrary(new Book("test", "test", "test", 10));
